daf7a300126fd3316a6f16a62bd0021780bc5750
light <pch1998007@gmail.com>
2021-07-14 13:04:35 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/DependInfo.cmake
@@ -0,0 +1,84 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/lightp/stm32/project/NUCLEO-H7/startup/startup_stm32h743xx.s" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/startup/startup_stm32h743xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32H743xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32H7xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/lightp/stm32/project/NUCLEO-H7/Core/Src/eth.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Core/Src/eth.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Core/Src/gpio.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Core/Src/gpio.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Core/Src/main.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Core/Src/main.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Core/Src/stm32h7xx_hal_msp.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Core/Src/stm32h7xx_hal_msp.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Core/Src/stm32h7xx_it.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Core/Src/stm32h7xx_it.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Core/Src/syscalls.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Core/Src/syscalls.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Core/Src/system_stm32h7xx.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Core/Src/system_stm32h7xx.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Core/Src/usart.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Core/Src/usart.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Core/Src/usb_otg.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Core/Src/usb_otg.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_eth.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_eth.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_eth_ex.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_eth_ex.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd_ex.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd_ex.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c.obj"
+  "/home/lightp/stm32/project/NUCLEO-H7/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_ll_usb.c" "/home/lightp/stm32/project/NUCLEO-H7/cmake-build-debug/CMakeFiles/NUCLEO-H7.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_ll_usb.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32H743xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32H7xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
